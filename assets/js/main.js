dfbe38143faee18176e149b6e33bcc9c0a382217
lorenzo-neri <lorenzo21neri@gmail.com>
2023-09-04 17:31:56 +0200

    try to add to change quantity of cell

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,11 +29,12 @@ const btnReset = document.querySelector('#btn_reset');
 //const grid
 const grid = document.querySelector('.grid');
 
+//select difficulty el
+let selectDifficultyEl = document.querySelector('select');
 
-
-
-
-const limit = 100;
+//limit
+const limit = changeDifficulty();
+console.log(limit);
 
 //const bombs
 const bombsNumber = 16;
@@ -45,6 +46,8 @@ btnPlay.addEventListener('click', function (e) {
 
     btnPlay.style.display = 'none';
     btnReset.style.display = 'inline-block';
+    selectDifficultyEl.style.display = 'none';
+
     const gridEl = document.querySelector('.grid');
     generateGrid(gridEl, limit);
 
@@ -55,6 +58,8 @@ btnPlay.addEventListener('click', function (e) {
 btnReset.addEventListener('click', function (e) {
     btnPlay.style.display = 'inline-block';
     btnReset.style.display = 'none';
+    selectDifficultyEl.style.display = 'inline-block';
+
 
     for (let i = 0; i < limit; i++) {
 
@@ -74,24 +79,51 @@ function generateGrid(domEl, limit) {
     //genero la griglia 
     for (let i = 0; i < limit; i++) {
 
-        const cellEl = document.createElement('div');
-        cellEl.className = 'cell';
-        cellEl.append(i + 1);
-        domEl.append(cellEl);
+        //const cellEl = document.createElement('div');
+        //cellEl.className = 'cell';
+        //cellEl.append(i + 1);
+        //domEl.append(cellEl);
         //console.log(cellEl);
 
-        //cliccando sulla cella, la cella si colora di azzurro ed emette un messaggio in console
+        //generate the grid
+        //Ogni cella ha un numero progressivo da 1 a 100.
+        const cellMarkupEl = generateCell(i + 1, 'div', 'cell', limit);
+
+        /* //cliccando sulla cella, la cella si colora di azzurro ed emette un messaggio in console
         cellEl.addEventListener('click', function () {
             //console.log('click on', cellEl);
             this.classList.toggle('if_click');
-        })
+        }) */
+
+        //append to the dom element
+        grid.append(cellMarkupEl);
+
     }
+}
+
+function generateCell(numb, el, css_class, limit) {
+
+    //console.log(this);
+
+    //generate the grid cell
+    const cellMarkupEl = document.createElement(el);
+    cellMarkupEl.append(numb);
+    cellMarkupEl.classList.add(css_class);
+    cellMarkupEl.style.width = `calc(100% / ${Math.sqrt(limit)})`;
+
+    //Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+    cellMarkupEl.addEventListener('click', function (ev) {
+        console.log(this);
+        this.classList.toggle('if_click');
+        console.log(this.innerText);
+    })
+
+    return cellMarkupEl
 }
 
 function changeDifficulty() {
 
     //const difficulty
-    const difficulty = document.getElementById('difficulty').value;
-    
+    const difficulty = Number(document.getElementById('difficulty').value);
     console.log(difficulty);
 }
